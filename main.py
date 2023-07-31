c04010f408f74e692a2e6f53a50a6a7cd94ce95e
Vladimir <nightsense@mail.ru>
2023-08-01 02:48:22 +0300

    fffff

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import json
 
 bot= Bot('6354377326:AAFwnePBEuiEV3zZP5JRVFEphc1UfcXw6jw')
 dp=Dispatcher(bot)
-
+admin='670976831'
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     markup=types.ReplyKeyboardMarkup()
@@ -14,7 +14,7 @@ async def start(message: types.Message):
 @dp.message_handler(content_types=['web_app_data'])
 async def web_app(message :types.Message):
     res=json.loads(message.web_app_data.data)
-    await message.answer(f'Streamer: {res["name"]}. Nickname: {res["name2"]}. Telegram: {res["utelegram"]}')
+    await bot.send_message(message.admin, f'Streamer: {res["name"]}. Nickname: {res["name2"]}. Telegram: {res["utelegram"]}')
 
 
 executor.start_polling(dp)
